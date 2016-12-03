740174a57e40abaee7be8f1210add361e9478942
david wright <david_v_wright@yahoo.com>
2016-12-03 01:23:13 -0800

    adding response method approach

--- xss.go
+++ xss.go
@@ -23,12 +23,6 @@ import (
 	"strconv"
 )
 
-//type nopCloser struct {
-//	io.Reader
-//}
-//
-//func (nopCloser) Close() error { return nil }
-
 // GinXSSMiddleware provides an 'auto' remove XSS malicious from all submitted user input.
 // e.g. POST and PUT
 // it's highly configurable.
@@ -52,35 +46,6 @@ type GinXSSMiddleware struct {
 
 }
 
-//// MiddlewareInit initialize jwt configs.
-//func (mw *GinXSSMiddleware) MiddlewareInit() error {
-//
-//	if mw.TokenLookup == "" {
-//		mw.TokenLookup = "header:Authorization"
-//	}
-//
-//	if mw.Authorizator == nil {
-//		mw.Authorizator = func(userID string, c *gin.Context) bool {
-//			return true
-//		}
-//	}
-//
-//	if mw.Unauthorized == nil {
-//		mw.Unauthorized = func(c *gin.Context, code int, message string) {
-//			c.JSON(code, gin.H{
-//				"code":    code,
-//				"message": message,
-//			})
-//		}
-//	}
-//
-//	if mw.Key == nil {
-//		return errors.New("secret key is required")
-//	}
-//
-//	return nil
-//}
-
 // MiddlewareFunc makes GinXSSMiddleware implement the Middleware interface.
 func (mw *GinXSSMiddleware) MiddlewareFunc() gin.HandlerFunc {
 	//if err := mw.MiddlewareInit(); err != nil {
@@ -101,132 +66,14 @@ func (mw *GinXSSMiddleware) middlewareImpl(c *gin.Context) {
 	err := mw.filterData(c)
 
 	if err != nil {
-		//mw.exception(c, http.StatusUnauthorized, err.Error())
-		//c.Abort()
-		//return
 		c.Abort()
 		return
 	}
 
-	//token, err := mw.parseToken(c)
-
-	//if err != nil {
-	//	mw.unauthorized(c, http.StatusUnauthorized, err.Error())
-	//	return
-	//}
-
-	//claims := token.Claims.(jwt.MapClaims)
-
-	//id := claims["id"].(string)
-	//c.Set("JWT_PAYLOAD", claims)
-	//c.Set("userID", id)
-
-	//if !mw.Authorizator(id, c) {
-	//	mw.unauthorized(c, http.StatusForbidden, "You don't have permission to access.")
-	//	return
-	//}
-
 	c.Next()
 }
 
-//func (mw *GinXSSMiddleware) jwtFromHeader(c *gin.Context, key string) (string, error) {
-//	authHeader := c.Request.Header.Get(key)
-//
-//	if authHeader == "" {
-//		return "", errors.New("auth header empty")
-//	}
-//
-//	parts := strings.SplitN(authHeader, " ", 2)
-//	if !(len(parts) == 2 && parts[0] == "Bearer") {
-//		return "", errors.New("invalid auth header")
-//	}
-//
-//	return parts[1], nil
-//}
-//
-//func (mw *GinXSSMiddleware) jwtFromQuery(c *gin.Context, key string) (string, error) {
-//	token := c.Query(key)
-//
-//	if token == "" {
-//		return "", errors.New("Query token empty")
-//	}
-//
-//	return token, nil
-//}
-//
-//func (mw *GinXSSMiddleware) jwtFromCookie(c *gin.Context, key string) (string, error) {
-//	cookie, _ := c.Cookie(key)
-//
-//	if cookie == "" {
-//		return "", errors.New("Cookie token empty")
-//	}
-//
-//	return cookie, nil
-//}
-//
-//func (mw *GinXSSMiddleware) parseToken(c *gin.Context) (*jwt.Token, error) {
-//	var token string
-//	var err error
-//
-//	parts := strings.Split(mw.TokenLookup, ":")
-//	switch parts[0] {
-//	case "header":
-//		token, err = mw.jwtFromHeader(c, parts[1])
-//	case "query":
-//		token, err = mw.jwtFromQuery(c, parts[1])
-//	case "cookie":
-//		token, err = mw.jwtFromCookie(c, parts[1])
-//	}
-//
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
-//		if jwt.GetSigningMethod(mw.SigningAlgorithm) != token.Method {
-//			return nil, errors.New("invalid signing algorithm")
-//		}
-//
-//		return mw.Key, nil
-//	})
-//}
-//
-//func (mw *GinXSSMiddleware) unauthorized(c *gin.Context, code int, message string) {
-//
-//	if mw.Realm == "" {
-//		mw.Realm = "gin jwt"
-//	}
-//
-//	c.Header("WWW-Authenticate", "JWT realm="+mw.Realm)
-//	c.Abort()
-//
-//	mw.Unauthorized(c, code, message)
-//
-//	return
-//}
-
-func (mw *GinXSSMiddleware) exception(c *gin.Context, code int, message string) {
-
-	//c.Header("WWW-Authenticate", "JWT realm="+mw.Realm)
-	c.Abort()
-
-	//mw.Unauthorized(c, code, message)
-
-	return
-}
-
 // Remove XSS
-//<nil>"PUT /api/v1/projects/1 HTTP/1.1\r\nHost: local.hubtones.com:11062\r\n
-//Accept: application/json\r\nAccept-Encoding: gzip, deflate, sdch\r\n
-// Accept-Language: en-US,en;q=0.8\r\nAuthorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0ODA2NzM2NDQsImlkIjoiVGVzdCBVc2VyIMKpIiwib3JpZ19pYXQiOjE0ODA0MDc2MzEsInVzZXJfaWQiOjUzNywidXNlcm5hbWUiOiJUZXN0IFVzZXIgwqkifQ.vYIdRkO6c4oatRZ4-gUL068RlWam-XO1XZrU6SGA8U0\r\n
-// Connection: keep-alive\r\n
-// Content-Length: 283\r\n
-// Content-Type: application/json\r\n
-// Origin: http://local.hubtones.com\r\n
-// Referer: http://local.hubtones.com/project/1/edit\r\n
-// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36\r\n\r\n
-// {\"id\":1,\"name\":\"Project ß£áçkqùë Jâçqùë ¥ \",\"description\":\"Iñtërnâtiônàlizætiøn project\",\"status\":\"Recording\",\"genre\":\"7\",\"sub_genre\":\"77\",\"bpm\":\"117\",\"key\":\"E\",\"visibility\":\"Public\",\"created_by\":537,\"created_at\":1474448233,\"updated_by\":537,\"updated_at\":1480493923}"
-// [GIN] 2016/11/30 - 00:25:38 | 200 |    2.717269ms | 127.0.0.1 |   PUT     /api/v1/projects/1
 func (mw *GinXSSMiddleware) filterData(c *gin.Context) error {
 	//dump, derr := httputil.DumpRequest(c.Request, true)
 	//fmt.Print(derr)
@@ -302,14 +149,6 @@ func (mw *GinXSSMiddleware) filterData(c *gin.Context) error {
 			buff.Truncate(buff.Len() - 1) // remove last ','
 			buff.WriteString(`}`)
 
-			////encBuf, merr := json.Marshal(buff.String(), encOpts{quoted: true})
-			//encBuf, merr := json.Marshal(buff.String())
-			//if merr != nil {
-			//	fmt.Printf("%v", merr)
-			//}
-			//////fmt.Println(string(data))
-			////c.Request.Body = ioutil.NopCloser(bytes.NewBuffer([]byte(string(encBuf))))
-
 			bodOut := buff.String()
 
 			enc := json.NewEncoder(ioutil.Discard)
@@ -349,63 +188,3 @@ func (mw *GinXSSMiddleware) filterData(c *gin.Context) error {
 //		}
 //	})
 //}
-
-// func AddContext(next http.Handler) http.Handler {
-//   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//     log.Println(r.Method, "-", r.RequestURI)
-//     cookie, _ := r.Cookie("username")
-//     if cookie != nil {
-//       //Add data to context
-//       ctx := context.WithValue(r.Context(), "Username", cookie.Value)
-//       next.ServeHTTP(w, r.WithContext(ctx))
-//     } else {
-//       next.ServeHTTP(w, r)
-//     }
-//   })
-// }
-
-// var postCustomHandle map[string]interface{}
-// jsnErr := json.NewDecoder(c.Request.Body).Decode(&postCustomHandle)
-// if jsnErr == nil {
-//         proj_id := postCustomHandle["project_id"]
-//         talent_ids := postCustomHandle["talent_ids"]
-//         if proj_id == nil || talent_ids == nil {
-//                 c.JSON(http.StatusBadRequest, gin.H{"message": "required paramaters cannot be empty."})
-//                 return
-//         } else {
-//                 //project_id := postCustomHandle["proj_id"].(string)
-//                 project_id = proj_id.(string)
-//         }
-// } else {
-//         cglog.CGlog.Println(c.Request.Body)
-//         cglog.CGlog.Printf("%T", c.Request.Body)
-//         cglog.CGlog.Print(jsnErr)
-//         c.JSON(http.StatusBadRequest, gin.H{"message": "error handling parameters"})
-//         return
-// }
-
-//func (mw *GinXSSMiddleware) jwtFromHeader(c *gin.Context, key string) (string, error) {
-//	authHeader := c.Request.Header.Get(key)
-//
-//	if authHeader == "" {
-//		return "", errors.New("auth header empty")
-//	}
-//
-//	parts := strings.SplitN(authHeader, " ", 2)
-//	if !(len(parts) == 2 && parts[0] == "Bearer") {
-//		return "", errors.New("invalid auth header")
-//	}
-//
-//	return parts[1], nil
-//}
-//
-//func (mw *GinXSSMiddleware) jwtFromQuery(c *gin.Context, key string) (string, error) {
-//	token := c.Query(key)
-//
-//	if token == "" {
-//		return "", errors.New("Query token empty")
-//	}
-//
-//	return token, nil
-//}
-//
---
+++ xss_response.go
@@ -0,0 +1,187 @@
+package xss
+
+// XXX TODO
+// bluemonday!
+// also have option to accept XSS into the database and filter it out on display
+
+import (
+	//"errors"
+	//"net/http/httputil" // debugging
+	"net/http"
+	//"strings"
+	//"time"
+	//"html"
+	//"io"
+	//"io/ioutil"
+	//"net/url"
+	//"os"
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net"
+	//"net/http"
+	"strconv"
+	"strings"
+)
+
+//buff.WriteString(html.EscapeString(vv))
+
+//func (mw *GinXSSMiddleware) MiddlewareResponseFunc() gin.HandlerFunc {
+
+const (
+	noWritten     = -1
+	defaultStatus = 200
+)
+
+func XSSResponseHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("Content-Type")
+		fmt.Println("%#v", c)
+
+		callback := c.DefaultQuery("callback", "")
+		//fmt.Println("here1")
+		//if callback == "" {
+		//	callback = c.DefaultQuery("jsonp", "")
+		//	if callback == "" {
+		//		c.Next()
+		//		return
+		//	}
+		//}
+		var wb *responseBuffer
+		if w, ok := c.Writer.(gin.ResponseWriter); ok {
+			wb = NewResponseBuffer(w)
+			c.Writer = wb
+			c.Next()
+		} else {
+			c.Next()
+			return
+		}
+
+		if strings.Index(wb.Header().Get("Content-Type"), "/json") >= 0 {
+			status := wb.status
+			data := wb.Body.Bytes()
+			wb.Body.Reset()
+
+			resp := &jsonpResponse{
+				Meta: map[string]interface{}{"status": status},
+				Data: data,
+			}
+			fmt.Println("HERE1")
+			//fmt.Println(wb.Header().Get("Content-Type"))
+			for k, v := range wb.Header() {
+				resp.Meta[strings.ToLower(k)] = v[0]
+			}
+			resp.Meta["content-length"] = len(data)
+
+			fmt.Println("HERE2")
+			body, err := json.Marshal(resp)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			fmt.Println("HERE3")
+			fmt.Println(string(body))
+
+			wb.Body.Write([]byte(callback + "("))
+			wb.Body.Write(body)
+			wb.Body.Write([]byte(")"))
+
+			wb.Header().Set("Content-Type", "application/json")
+			wb.Header().Set("Content-Length", strconv.Itoa(wb.Body.Len()))
+		}
+
+		wb.Flush()
+	}
+}
+
+type jsonpResponse struct {
+	Meta map[string]interface{}
+	Data interface{}
+}
+
+type responseBuffer struct {
+	Response gin.ResponseWriter // the actual ResponseWriter to flush to
+	status   int                // the HTTP response code from WriteHeader
+	Body     *bytes.Buffer      // the response content body
+	Flushed  bool
+}
+
+func NewResponseBuffer(w gin.ResponseWriter) *responseBuffer {
+	return &responseBuffer{
+		Response: w, status: defaultStatus, Body: &bytes.Buffer{},
+	}
+}
+
+func (w *responseBuffer) Header() http.Header {
+	return w.Response.Header() // use the actual response header
+}
+
+func (w *responseBuffer) Write(buf []byte) (int, error) {
+	w.Body.Write(buf)
+	return len(buf), nil
+}
+
+func (w *responseBuffer) WriteString(s string) (n int, err error) {
+	//w.WriteHeaderNow()
+	//n, err = io.WriteString(w.ResponseWriter, s)
+	//w.size += n
+	n, err = w.Write([]byte(s))
+	return
+}
+
+func (w *responseBuffer) Written() bool {
+	return w.Body.Len() != noWritten
+}
+
+func (w *responseBuffer) WriteHeader(status int) {
+	w.status = status
+}
+
+func (w *responseBuffer) WriteHeaderNow() {
+	//if !w.Written() {
+	//    w.size = 0
+	//    w.ResponseWriter.WriteHeader(w.status)
+	//}
+}
+
+func (w *responseBuffer) Status() int {
+	return w.status
+}
+
+func (w *responseBuffer) Size() int {
+	return w.Body.Len()
+}
+
+func (w *responseBuffer) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	//if w.size < 0 {
+	//    w.size = 0
+	//}
+	return w.Response.(http.Hijacker).Hijack()
+}
+
+func (w *responseBuffer) CloseNotify() <-chan bool {
+	return w.Response.(http.CloseNotifier).CloseNotify()
+}
+
+// Fake Flush
+// TBD
+func (w *responseBuffer) Flush() {
+	w.realFlush()
+}
+
+func (w *responseBuffer) realFlush() {
+	if w.Flushed {
+		return
+	}
+	w.Response.WriteHeader(w.status)
+	if w.Body.Len() > 0 {
+		_, err := w.Response.Write(w.Body.Bytes())
+		if err != nil {
+			panic(err)
+		}
+		w.Body.Reset()
+	}
+	w.Flushed = true
+}
